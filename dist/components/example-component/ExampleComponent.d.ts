4ffed91933f8ee44b7ac2cc2204549a9d67d471d
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2023-11-17 13:45:37 +0100

    Add dist build folder

---
+++ dist/components/example-component/ExampleComponent.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="react" />
+interface Props {
+    a: number;
+    b: boolean;
+    c: string;
+}
+export declare function ExampleComponent(props: Props): JSX.Element;
+export {};
+//# sourceMappingURL=ExampleComponent.d.ts.map
