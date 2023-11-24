4b677101d5af19ce778807446d15516759763f5d
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2023-11-24 11:19:19 +0100

    DS-177 Update all license headers

--- config/jest/SetupTestEnvironment.ts
+++ config/jest/SetupTestEnvironment.ts
@@ -1,6 +1,6 @@
 /*
- * SonarQube
- * Copyright (C) 2009-2023 SonarSource SA
+ * Echoes React
+ * Copyright (C) 2023-2023 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
