61918bed6d61c3afa47b53cd2f52331066fc6c99
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2023-11-24 14:53:23 +0100

    DS-178 Apply feedback

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,51 +1,33 @@
 module.exports = {
   coverageDirectory: '<rootDir>/test-results/coverage',
-  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*-stories.tsx', '!src/**/index.ts'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/generated/*',
+    '!src/**/*-stories.tsx',
+    '!src/**/index.ts',
+  ],
   coverageReporters: ['lcovonly', 'text'],
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   moduleNameMapper: {
-    '^.+\\.(md|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/config/jest/FileStub.js',
-    '^.+\\.css$': '<rootDir>/config/jest/CSSStub.js',
+    '^~common/(.*)': '<rootDir>/src/common/$1',
+    '^~generated/(.*)': '<rootDir>/src/generated/$1',
+    '^~utils/(.*)': '<rootDir>/src/utils/$1',
   },
   setupFiles: ['<rootDir>/config/jest/SetupTestEnvironment.ts'],
-  setupFilesAfterEnv: [
-    '<rootDir>/config/jest/SetupReactTestingLibrary.ts',
-    '<rootDir>/config/jest/SetupJestAxe.ts',
-  ],
+  setupFilesAfterEnv: ['<rootDir>/config/jest/SetupJestAxe.ts'],
   snapshotSerializers: ['@emotion/jest/serializer'],
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: [
     '<rootDir>/config',
-    '<rootDir>/design-system',
+    '<rootDir>/design-tokens',
     '<rootDir>/node_modules',
     '<rootDir>/scripts',
+    '<rootDir>/dist',
+    '<rootDir>/stories',
+    '<rootDir>/src/generated',
   ],
-  testRegex: '(/__tests__/.*|\\-test)\\.(ts|tsx)$',
+  testRegex: '(/__tests__/.*|\\-test)\\.(t|j)sx?$',
   transform: {
-    '^.+\\.(t|j)sx?$': [
-      '@swc/jest',
-      {
-        jsc: {
-          target: 'es2018',
-        },
-      },
-    ],
+    '^.+\\.(t|j)sx?$': '@swc/jest',
   },
-  transformIgnorePatterns: ['/node_modules/(?!(d3-.+))/'],
-  reporters: [
-    'default',
-    [
-      'jest-junit',
-      {
-        outputDirectory: 'test-results/',
-        outputName: 'junit.xml',
-        ancestorSeparator: ' > ',
-        suiteNameTemplate: '{filename}',
-        classNameTemplate: '{classname}',
-        titleTemplate: '{title}',
-      },
-    ],
-  ],
-  testTimeout: 60000,
 };
