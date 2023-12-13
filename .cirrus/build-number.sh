e5864bf176cb87f00779904e990cd7f1c502fcbb
Lucas Paulger <lucas.paulger@sonarsource.com>
2023-12-13 13:36:23 +0200

    remove build_number task and combine into the publish script

--- .cirrus/build-number.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-set -x
-
-source cirrus-env BUILD
-export VERSION=$(grep version package.json | head -1 | awk -F: '{ print $2 }' | sed 's/[", ]//g')
-
-echo "VERSION=$VERSION" >> $CIRRUS_ENV
--- .cirrus/publish.sh
+++ .cirrus/publish.sh
@@ -3,6 +3,9 @@
 set -euo pipefail
 
 PROJECT=echoes-react
+VERSION=$(grep version package.json | head -1 | awk -F: '{ print $2 }' | sed 's/[", ]//g')
+
+source cirrus-env BUILD
 
 npm version --no-git-tag-version --allow-same-version "$VERSION-$BUILD_NUMBER"
 
