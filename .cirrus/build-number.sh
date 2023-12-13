19b96637a82a104411676d9c4969ac0f2086e21d
Lucas Paulger <lucas.paulger@sonarsource.com>
2023-12-13 12:14:40 +0200

    export the Version properly

--- .cirrus/build-number.sh
+++ .cirrus/build-number.sh
@@ -4,4 +4,6 @@ set -euo pipefail
 set -x
 
 source cirrus-env BUILD
-VERSION=$(grep version package.json | head -1 | awk -F: '{ print $2 }' | sed 's/[", ]//g')
+export VERSION=$(grep version package.json | head -1 | awk -F: '{ print $2 }' | sed 's/[", ]//g')
+
+echo "VERSION=$VERSION" >> $CIRRUS_ENV
