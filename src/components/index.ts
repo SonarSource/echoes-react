2dd7674c0b1796407d3f6eac663d32be41cb780b
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2024-02-27 09:07:10 -0600

    DS-173 Prepare release version 0.1.0

--- src/components/index.ts
+++ src/components/index.ts
@@ -18,7 +18,5 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-export * from './checkbox';
 export * from './links';
-export * from './radio-button-group';
 export * from './spinner';
