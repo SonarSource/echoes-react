ecf2ae05186d738708ade25db50e39829e63f3ed
Jeremy Davis <jeremy.davis@sonarsource.com>
2024-06-28 10:52:30 +0200

    DS-369 Add test for Async

--- src/components/select/SelectAsync.tsx
+++ src/components/select/SelectAsync.tsx
@@ -24,7 +24,7 @@ import { PropsWithLabels } from '~types/utils';
 import { SelectBase, SelectBaseProps } from './SelectCommons';
 import { SelectOption } from './SelectTypes';
 
-interface Props extends SelectBaseProps {
+interface Props extends Omit<SelectBaseProps, 'filter' | 'isSearchable'> {
   onSearch: MantineSelectProps['onSearchChange'];
 }
 
@@ -33,6 +33,7 @@ export const SelectAsync = forwardRef<HTMLInputElement, PropsWithLabels<Props>>(
     data,
     onChange,
     onSearch,
+    value,
 
     ...selectProps
   } = props;
@@ -83,10 +84,12 @@ export const SelectAsync = forwardRef<HTMLInputElement, PropsWithLabels<Props>>(
   return (
     <SelectBase
       data={data}
+      filter={() => true} // Filtering is done on search
       isSearchable
       onChange={handleChange}
       onSearch={handleSearch}
       ref={ref}
+      value={value ?? null}
       {...selectProps}
     />
   );
