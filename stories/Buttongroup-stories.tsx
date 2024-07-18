444e1155bfbe96f5117f3aa749bab9c507d530b1
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2024-07-18 14:42:49 +0200

    DS-504 Implement ButtonGroup

---
+++ stories/Buttongroup-stories.tsx
@@ -0,0 +1,59 @@
+/*
+ * Echoes React
+ * Copyright (C) 2023-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button, ButtonGroup } from '../src';
+import { basicWrapperDecorator } from './helpers/BasicWrapper';
+
+const meta: Meta<typeof ButtonGroup> = {
+  component: ButtonGroup,
+  title: 'Echoes/ButtonGroup',
+  parameters: {
+    controls: { exclude: ['children'] },
+  },
+  decorators: [basicWrapperDecorator],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ButtonGroup>;
+
+const children = (
+  <>
+    <Button>First</Button>
+    <Button>Second</Button>
+    <Button>Third</Button>
+    <Button>Fourth</Button>
+  </>
+);
+
+export const NotCombined: Story = {
+  args: {
+    children,
+    isCombined: false,
+  },
+};
+
+export const Combined: Story = {
+  args: {
+    children,
+    isCombined: true,
+  },
+};
