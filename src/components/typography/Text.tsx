5429b8182d113ddf65907594ce3b15c8cd9928a5
Jeremy Davis <jeremy.davis@sonarsource.com>
2024-09-16 18:01:22 +0200

    DS-520 New 'Text' component

---
+++ src/components/typography/Text.tsx
@@ -0,0 +1,107 @@
+/*
+ * Echoes React
+ * Copyright (C) 2023-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import styled from '@emotion/styled';
+import { CSSProperties, forwardRef, PropsWithChildren } from 'react';
+import { isDefined } from '~common/helpers/types';
+import { DesignTokensColorsText } from '~types/design-tokens';
+
+export enum TextSize {
+  Small = 'small',
+  Default = 'default',
+  Large = 'large',
+}
+
+type Props = {
+  as?: 'span' | 'p' | 'div';
+  className?: string;
+  isStrong?: boolean;
+  size?: TextSize;
+} & ColorProps;
+
+type ColorProps =
+  | {
+      colorOverride?: DesignTokensColorsText;
+      isSubdued?: never;
+    }
+  | {
+      colorOverride?: never;
+      isSubdued: boolean;
+    };
+
+export const Text = forwardRef<HTMLSpanElement, PropsWithChildren<Props>>((props, ref) => {
+  const {
+    children,
+    colorOverride,
+    isStrong = false,
+    isSubdued = false,
+    size = TextSize.Default,
+    ...restAndRadixProps
+  } = props;
+
+  const additionalProps: { style?: CSSProperties } = isDefined(colorOverride)
+    ? { style: { color: `var(--${colorOverride})` } }
+    : {};
+
+  return (
+    <StyledText
+      isStrong={isStrong}
+      isSubdued={isSubdued}
+      ref={ref}
+      size={size}
+      {...additionalProps}
+      {...restAndRadixProps}>
+      {children}
+    </StyledText>
+  );
+});
+Text.displayName = 'Text';
+
+type StyledTextProps = Required<Pick<Props, 'isSubdued' | 'isStrong' | 'size'>>;
+
+const StyledText = styled.span<StyledTextProps>`
+  max-width: var(--echoes-sizes-typography-max-width-default);
+  font: ${getFontForSizeAndWeight};
+
+  color: ${getColor};
+`;
+
+function getColor({ isSubdued }: Pick<StyledTextProps, 'isSubdued'>) {
+  return isSubdued ? 'var(--echoes-color-text-subdued)' : 'var(--echoes-color-text-default)';
+}
+
+function getFontForSizeAndWeight({ size, isStrong }: Pick<StyledTextProps, 'size' | 'isStrong'>) {
+  return TYPOGRAPHY_MAP[`${isStrong}`][size];
+}
+
+const TYPOGRAPHY_MAP = {
+  /* isStrong is true */
+  true: {
+    [TextSize.Small]: 'var(--echoes-typography-text-small-semi-bold)',
+    [TextSize.Default]: 'var(--echoes-typography-text-default-semi-bold)',
+    [TextSize.Large]: 'var(--echoes-typography-text-large-semi-bold)',
+  },
+  /* isStrong is false */
+  false: {
+    [TextSize.Small]: 'var(--echoes-typography-text-small-medium)',
+    [TextSize.Default]: 'var(--echoes-typography-text-default-regular)',
+    [TextSize.Large]: 'var(--echoes-typography-text-large-regular)',
+  },
+};
