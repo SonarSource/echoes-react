6fdc369390e5714ef56903411a6d0d7be993111d
Jeremy Davis <jeremy.davis@sonarsource.com>
2024-10-31 17:26:25 +0100

    DS-698 New brand logos

--- src/components/logos/index.ts
+++ src/components/logos/index.ts
@@ -18,17 +18,9 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-export * from './buttons';
-export * from './checkbox';
-export * from './dropdown-menu';
-export * from './echoes-provider';
-export * from './icons';
-export * from './links';
-export * from './logos';
-export * from './modals';
-export * from './popover';
-export * from './radio-button-group';
-export * from './select';
-export * from './spinner';
-export * from './tooltip';
-export * from './typography';
+export { LogoSonar } from './LogoSonar';
+export { LogoSonarQubeCloud } from './LogoSonarQubeCloud';
+export { LogoSonarQubeCommunity } from './LogoSonarQubeCommunity';
+export { LogoSonarQubeIde } from './LogoSonarQubeIde';
+export { LogoSonarQubeServer } from './LogoSonarQubeServer';
+export { LogoSize } from './logoUtils';
---
+++ stories/Logos-stories.tsx
@@ -0,0 +1,80 @@
+/*
+ * Echoes React
+ * Copyright (C) 2023-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+/* eslint-disable no-console */
+import styled from '@emotion/styled';
+import type { Meta, StoryObj } from '@storybook/react';
+import {
+  LogoSize,
+  LogoSonar,
+  LogoSonarQubeCloud,
+  LogoSonarQubeCommunity,
+  LogoSonarQubeIde,
+  LogoSonarQubeServer,
+} from '../src/components/logos';
+import { basicWrapperDecorator } from './helpers/BasicWrapper';
+
+const meta: Meta = {
+  title: 'Echoes/Logos',
+  decorators: [basicWrapperDecorator],
+  argTypes: {
+    hasText: {
+      control: 'boolean',
+    },
+    size: {
+      control: { type: 'select' },
+      options: [LogoSize.Small, LogoSize.Medium, LogoSize.Large],
+    },
+  },
+  args: {
+    hasText: true,
+  },
+};
+
+export default meta;
+
+interface StoryArgs {
+  hasText?: boolean;
+}
+
+export const SonarQubeLogos: StoryObj<StoryArgs> = {
+  render: (args) => (
+    <Wrapper>
+      <LogoSonar {...args} />
+
+      <LogoSonarQubeCloud {...args} />
+
+      <LogoSonarQubeCommunity {...args} />
+
+      <LogoSonarQubeIde {...args} />
+
+      <LogoSonarQubeServer {...args} />
+    </Wrapper>
+  ),
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+  margin: 1rem;
+  align-items: start;
+  width: 210px;
+`;
