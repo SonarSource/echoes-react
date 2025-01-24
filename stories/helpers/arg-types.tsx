b19c089a02671b24716b871f7af14f57667a81cb
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2025-01-24 15:36:57 +0100

    ECHOES-64 Implement TextInput component

--- stories/helpers/arg-types.tsx
+++ stories/helpers/arg-types.tsx
@@ -17,4 +17,24 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-export { TextInput } from './TextInput';
+
+import * as icons from '../../src/components/icons';
+
+const iconKeys = Object.keys(icons) as (keyof typeof icons)[];
+
+const iconsElements = Object.fromEntries(
+  iconKeys.map((key: keyof typeof icons) => {
+    const IconComponent = icons[key];
+    return [key, <IconComponent key={key} />];
+  }),
+);
+
+export const iconsElementsArgType = {
+  mapping: iconsElements,
+  options: iconKeys,
+};
+
+export const iconsComponentsArgType = {
+  mapping: icons,
+  options: iconKeys,
+};
