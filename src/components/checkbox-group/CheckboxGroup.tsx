44c33b666626f5686419a45976a8001a5acb681d
Daniel Nagy <1622446+daniel-nagy@users.noreply.github.com>
2025-01-28 16:18:28 -0600

    add jsdoc comments

--- src/components/checkbox-group/CheckboxGroup.tsx
+++ src/components/checkbox-group/CheckboxGroup.tsx
@@ -30,7 +30,43 @@ import {
 import { useFormFieldA11y } from '../form/useFormFieldA11y';
 
 /**
+ * A checkbox group allows a user to select multiple items from a list of
+ * options. A checkbox group must have at least one option.
  *
+ * Each option must have a unique value. If no value is provided for an option,
+ * then the option's label will be used as its value.
+ *
+ * The selected values may appear in any order. Equality is determined using
+ * strict equality (`===`).
+ *
+ * The disabled state and error state will be applied to the entire group.
+ * However, the disabled state or error state can be overridden per option.
+ *
+ * > A checkbox group is a controlled element, it requires a `value` and
+ * `onChange` handler.
+ *
+ * **Permitted Content**
+ *
+ * None; it's a self closing element.
+ *
+ * **Permitted Parents**
+ *
+ * Any element that accepts flow content.
+ *
+ * **Example**
+ *
+ * ```tsx
+ * <CheckboxGroup
+ *   label="Favorite colors"
+ *   onChange={setSelectedColors}
+ *   options={[
+ *     { label: 'Red' },
+ *     { label: 'Green' },
+ *     { label: 'Blue' },
+ *   ]}
+ *   value={selectedColors}
+ * />
+ * ```
  */
 export const CheckboxGroup: CheckboxGroup = forwardRef<HTMLDivElement, CheckboxGroupProps>(
   (props, ref) => {
@@ -54,11 +90,12 @@ export const CheckboxGroup: CheckboxGroup = forwardRef<HTMLDivElement, CheckboxG
     } = props;
     const defaultId = `${useId()}checkbox`;
 
-    const { describedBy, descriptionId, labelId, validationMessageId } = useFormFieldA11y({
-      controlId: id ?? defaultId,
-      hasDescription: Boolean(helpText),
-      hasValidationMessage: Boolean(messageInvalid || messageValid),
-    });
+    const { controlId, describedBy, descriptionId, labelId, validationMessageId } =
+      useFormFieldA11y({
+        controlId: id ?? defaultId,
+        hasDescription: Boolean(helpText),
+        hasValidationMessage: Boolean(messageInvalid || messageValid),
+      });
 
     return (
       <FormField
@@ -80,20 +117,21 @@ export const CheckboxGroup: CheckboxGroup = forwardRef<HTMLDivElement, CheckboxG
           aria-labelledby={ariaLabelledBy ?? labelId}
           data-direction={direction}
           data-invalid={validation === FormFieldValidation.Invalid}
+          id={controlId}
           ref={ref}
           role="group">
           {options.map((option) => {
+            const { value: _value, ...rest } = option;
             const optionValue = 'value' in option ? option.value : option.label;
-            const isChecked = value.includes(optionValue);
 
             return (
               <Checkbox
-                checked={isChecked}
+                checked={value.includes(optionValue)}
                 hasError={validation === FormFieldValidation.Invalid}
                 isDisabled={isDisabled}
                 key={option.label}
                 onCheck={(checkedState) => {
-                  if (option.isDisabled) {
+                  if (isDisabled || option.isDisabled) {
                     return;
                   }
 
@@ -106,7 +144,7 @@ export const CheckboxGroup: CheckboxGroup = forwardRef<HTMLDivElement, CheckboxG
                       break;
                   }
                 }}
-                {...option}
+                {...rest}
               />
             );
           })}
@@ -135,24 +173,71 @@ type CheckboxOption<T = unknown> = Pick<
   | 'onFocus'
   | 'title'
 > & {
+  /**
+   * The label displayed next to the checkbox.
+   */
   label: string;
+  /**
+   * Explicitly set the value of the checkbox (optional). If not provided, the
+   * label is used as the checkbox value.
+   */
   value?: T;
 };
 
+/**
+ * Options for how checkboxes are displayed in a group.
+ */
 export enum CheckboxGroupDirection {
+  /**
+   * Display checkboxes in a row.
+   */
   Horizontal = 'horizontal',
+  /**
+   * Display checkboxes in a column.
+   */
   Vertical = 'vertical',
 }
 
 interface CheckboxGroupPropsBase<T> extends RefAttributes<HTMLDivElement>, ValidationProps {
+  /**
+   * Add a `class` attribute to the root element (optional).
+   */
   className?: string;
+  /**
+   * The direction the checkboxes are laid out in the group (optional). The default is `vertical`.
+   */
   direction?: `${CheckboxGroupDirection}`;
+  /**
+   * The ID of the element with the role `group` (optional).
+   */
   id?: string;
+  /**
+   * Prevent the user from interacting with the checkboxes (optional).
+   */
   isDisabled?: boolean;
+  /**
+   * When true, an asterisk will be displayed next to the label to indicate that
+   * the field is required.
+   */
   isRequired?: boolean;
+  /**
+   * Called when the value of the group changes.
+   */
   onChange(value: T[]): void;
+  /**
+   * Defines which checkboxes are available in the group. Each checkbox should
+   * have a unique value.
+   */
   options: [CheckboxOption<T>, ...CheckboxOption<T>[]];
+  /**
+   * An unordered list of the selected values. The value of each checkbox is
+   * assumed to be unique.
+   */
   value: T[];
+  /**
+   * Controls the width of the form field (optional). The default value is
+   * `full`, meaning it will take up the full width of its container.
+   */
   width?: `${FormFieldWidth}`;
 }
 
