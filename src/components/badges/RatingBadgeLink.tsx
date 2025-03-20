9d97459b5abef9f359dd34eedd891a87ce3d5d8d
David Cho-Lerat <david.cho-lerat@sonarsource.com>
2025-03-20 11:27:12 +0100

    ECHOES-647,648,649 Implement RatingBadge components

---
+++ src/components/badges/RatingBadgeLink.tsx
@@ -0,0 +1,51 @@
+/*
+ * Echoes React
+ * Copyright (C) 2023-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import styled from '@emotion/styled';
+import { forwardRef } from 'react';
+import { LinkProps, LinkStandalone } from '../links';
+import { RatingBadge, RatingBadgeProps } from './RatingBadge';
+
+export type RatingBadgeLinkProps = Omit<LinkProps, 'children' | 'highlight'> & RatingBadgeProps;
+
+export const RatingBadgeLink = forwardRef<HTMLAnchorElement, RatingBadgeLinkProps>(
+  ({ className, rating, size, style, ...linkProps }, ref) => (
+    <LinkStandaloneStyled ref={ref} {...linkProps}>
+      <RatingBadge {...{ className, rating, size, style }} />
+    </LinkStandaloneStyled>
+  ),
+);
+
+RatingBadgeLink.displayName = 'RatingBadgeLink';
+
+const LinkStandaloneStyled = styled(LinkStandalone)`
+  & div:hover {
+    text-decoration-line: var(--echoes-text-decoration-underline);
+  }
+
+  &:focus-visible {
+    outline: none; // remove the one from LinkBaseStyled
+
+    & div {
+      outline: var(--echoes-color-focus-default) solid var(--echoes-focus-border-width-default);
+      outline-offset: var(--echoes-focus-border-offset-default);
+    }
+  }
+`;
