880d34d38c700193e20ea13ccb89ed1795f74471
robert-hoover-sonarsource <robert.hoover@sonarsource.com>
2025-03-25 09:44:53 -0500

    ECHOES-651 Implement Divider Component (#299)

---
+++ src/components/divider/Divider.tsx
@@ -0,0 +1,163 @@
+/*
+ * Echoes React
+ * Copyright (C) 2023-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import styled from '@emotion/styled';
+import { AriaRole, forwardRef } from 'react';
+
+export interface DividerProps {
+  /**
+   * Additional CSS class name
+   */
+  className?: string;
+
+  /**
+   * Optional data-testid for testing
+   */
+  'data-testid'?: string;
+
+  /**
+   * Whether the divider is vertical
+   * @default false
+   */
+  isVertical?: boolean;
+
+  /**
+   * Optional ARIA role. If not provided, no role will be applied.
+   * For semantic separation, 'separator' is recommended.
+   */
+  role?: AriaRole;
+
+  /**
+   * Optional text to display in the middle of the divider
+   */
+  text?: React.ReactNode;
+}
+
+export const Divider = forwardRef<HTMLHRElement | HTMLDivElement, Readonly<DividerProps>>(
+  (
+    {
+      className,
+      'data-testid': dataTestId,
+      isVertical = false,
+      role = 'separator',
+      text,
+      ...props
+    },
+    ref,
+  ) => {
+    const ariaOrientation = isVertical ? 'vertical' : 'horizontal';
+
+    if (!text) {
+      return (
+        <StyledDivider
+          aria-orientation={ariaOrientation}
+          className={className}
+          data-testid={dataTestId}
+          isVertical={isVertical}
+          ref={ref as React.Ref<HTMLHRElement>}
+          role={role}
+          {...props}
+        />
+      );
+    }
+
+    return (
+      <DividerContainer
+        aria-orientation={ariaOrientation}
+        className={className}
+        data-testid={dataTestId}
+        isVertical={isVertical}
+        ref={ref}
+        role={role}
+        {...props}>
+        <DividerLine isVertical={isVertical} />
+        <DividerText isVertical={isVertical}>{text}</DividerText>
+        <DividerLine isVertical={isVertical} />
+      </DividerContainer>
+    );
+  },
+);
+
+const StyledDivider = styled.hr<{ isVertical: boolean }>`
+  background-color: var(--echoes-color-border-weak);
+  border: none;
+  margin: 0;
+
+  ${({ isVertical }) =>
+    !isVertical
+      ? `
+        height: 1px;
+        margin: var(--echoes-dimension-space-150) 0;
+        width: 100%;
+      `
+      : `
+        display: inline-block;
+        height: 100%;
+        margin: 0 var(--echoes-dimension-space-150);
+        min-height: 60px;
+        width: 1px;
+      `}
+`;
+
+const DividerContainer = styled.div<{ isVertical: boolean }>`
+  align-items: center;
+  display: flex;
+  margin: ${({ isVertical }) =>
+    !isVertical ? 'var(--echoes-dimension-space-100) 0' : '0 var(--echoes-dimension-space-100)'};
+
+  ${({ isVertical }) =>
+    isVertical
+      ? `
+        flex-direction: column;
+        height: 100%;
+        min-height: 60px;
+        width: auto;
+      `
+      : `
+        width: 100%;
+      `}
+`;
+
+const DividerLine = styled.div<{ isVertical: boolean }>`
+  background-color: var(--echoes-color-border-weak);
+  flex-grow: 1;
+
+  ${({ isVertical }) =>
+    !isVertical
+      ? `
+        height: 1px;
+      `
+      : `
+      height: 100%;
+      width: 1px;
+      `}
+`;
+
+const DividerText = styled.span<{ isVertical: boolean }>`
+  font-size: var(--echoes-font-size-10);
+  font-weight: var(--echoes-font-weight-regular);
+  line-height: var(--echoes-line-height-10);
+  max-width: var(--echoes-sizes-typography-max-width-default);
+  padding: ${({ isVertical }) =>
+    !isVertical ? '0 var(--echoes-dimension-space-25)' : 'var(--echoes-dimension-space-25) 0'};
+  white-space: nowrap;
+`;
+
+Divider.displayName = 'Divider';
---
+++ stories/Divider-stories.tsx
@@ -0,0 +1,146 @@
+/*
+ * Echoes React
+ * Copyright (C) 2023-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import type { Meta, StoryObj } from '@storybook/react';
+import styled from '@emotion/styled';
+import { basicWrapperDecorator } from './helpers/BasicWrapper';
+import { Divider, Heading, Text, TextSize } from '../src';
+
+const meta: Meta<typeof Divider> = {
+  component: Divider,
+  title: 'Echoes/Divider',
+  argTypes: {
+    isVertical: {
+      control: 'boolean',
+      description: 'Whether the divider is vertical',
+      defaultValue: false,
+    },
+    className: { control: 'text' },
+    role: { control: 'text' },
+    'data-testid': { control: 'text' },
+  },
+  decorators: [basicWrapperDecorator],
+  parameters: {
+    docs: {
+      description: {
+        component: 'A divider is a thin line that groups content in lists and layouts.',
+      },
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Divider>;
+
+const VerticalContainer = styled.div`
+  align-items: center;
+  display: flex;
+  height: 60px;
+  justify-content: space-between;
+`;
+
+interface DynamicContainerProps {
+  children: React.ReactNode;
+  isVertical?: boolean;
+}
+
+function DynamicContainer({ children, isVertical = false }: Readonly<DynamicContainerProps>) {
+  if (isVertical) {
+    return <VerticalContainer>{children}</VerticalContainer>;
+  }
+
+  return <div>{children}</div>;
+}
+
+export const Default: Story = {
+  args: {},
+  render: (args) => (
+    <DynamicContainer isVertical={args.isVertical ?? false}>
+      {args.isVertical ? (
+        <>
+          <span>Left content</span>
+          <Divider {...args} />
+          <span>Right content</span>
+        </>
+      ) : (
+        <>
+          <Heading as="h5" hasMarginBottom>
+            Is this a real issue?
+          </Heading>
+          <Text as="p">Probably</Text>
+          <Divider {...args} />
+          <Heading as="h5" hasMarginBottom>
+            What other things should we put here
+          </Heading>
+          <Text as="p">Super important text probably</Text>
+        </>
+      )}
+    </DynamicContainer>
+  ),
+};
+
+export const Vertical: Story = {
+  args: {
+    isVertical: true,
+  },
+  render: (args) => (
+    <DynamicContainer isVertical>
+      <span>Left content</span>
+      <Divider {...args} />
+      <span>Right content</span>
+    </DynamicContainer>
+  ),
+};
+
+export const WithText: Story = {
+  args: {
+    text: (
+      <Text isSubdued size={TextSize.Small}>
+        OR
+      </Text>
+    ),
+  },
+  render: (args) => (
+    <DynamicContainer>
+      <p>You can sign in with your credentials</p>
+      <Divider {...args} />
+      <p>Continue with social media</p>
+    </DynamicContainer>
+  ),
+};
+
+export const VerticalWithText: Story = {
+  args: {
+    isVertical: true,
+    text: (
+      <Text isSubdued size={TextSize.Small}>
+        OR
+      </Text>
+    ),
+  },
+  render: (args) => (
+    <DynamicContainer isVertical>
+      <span>Left content</span>
+      <Divider {...args} />
+      <span>Right content</span>
+    </DynamicContainer>
+  ),
+};
