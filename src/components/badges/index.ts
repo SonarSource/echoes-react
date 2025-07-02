17d4d00064f42f13985bc6bc773e04b613ced701
Jay <jeremy.davis@sonarsource.com>
2025-07-02 09:41:35 +0200

    ECHOES-820 Fix BadgeSeverity props & exports

--- src/components/badges/index.ts
+++ src/components/badges/index.ts
@@ -20,7 +20,7 @@
 
 export { Badge, BadgeSize, BadgeVariety } from './Badge';
 export { BadgeCounter } from './BadgeCounter';
-export { BadgeSeverity, BadgeSeverityLevel } from './BadgeSeverity';
+export { BadgeSeverity, BadgeSeverityLevel, BadgeSeverityVariety } from './BadgeSeverity';
 export { RatingBadge, RatingBadgeRating, RatingBadgeSize } from './RatingBadge';
 export { RatingBadgeButton } from './RatingBadgeButton';
 export { RatingBadgeLink } from './RatingBadgeLink';
