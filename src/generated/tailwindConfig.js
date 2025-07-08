69ae545860c388544db91ba096e5fe1e7dc37c77
Jay <jeremy.davis@sonarsource.com>
2025-07-08 09:10:02 +0200

    ECHOES-178 Tailwind preset & utilities for products

---
+++ src/generated/tailwindConfig.js
@@ -0,0 +1,133 @@
+
+/**
+ * Echoes React
+ * Copyright (C) 2023-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ * GENERATED FILE: do not edit directly.
+ */
+
+
+const config = {
+  "echoesPreset": {
+    "theme": {
+      "spacing": {
+        "0": "var(--echoes-dimension-space-0, 0px)",
+        "25": "var(--echoes-dimension-space-25, 0.125rem)",
+        "50": "var(--echoes-dimension-space-50, 0.25rem)",
+        "75": "var(--echoes-dimension-space-75, 0.375rem)",
+        "100": "var(--echoes-dimension-space-100, 0.5rem)",
+        "150": "var(--echoes-dimension-space-150, 0.75rem)",
+        "200": "var(--echoes-dimension-space-200, 1rem)",
+        "250": "var(--echoes-dimension-space-250, 1.25rem)",
+        "300": "var(--echoes-dimension-space-300, 1.5rem)",
+        "400": "var(--echoes-dimension-space-400, 2rem)",
+        "500": "var(--echoes-dimension-space-500, 2.5rem)",
+        "600": "var(--echoes-dimension-space-600, 3rem)",
+        "800": "var(--echoes-dimension-space-800, 4rem)"
+      },
+      "height": {
+        "100": "var(--echoes-dimension-height-100, 0.25rem)",
+        "150": "var(--echoes-dimension-height-150, 0.375rem)",
+        "200": "var(--echoes-dimension-height-200, 0.5rem)",
+        "400": "var(--echoes-dimension-height-400, 1rem)",
+        "500": "var(--echoes-dimension-height-500, 1.25rem)",
+        "600": "var(--echoes-dimension-height-600, 1.5rem)",
+        "800": "var(--echoes-dimension-height-800, 2rem)",
+        "900": "var(--echoes-dimension-height-900, 2.25rem)",
+        "1000": "var(--echoes-dimension-height-1000, 2.5rem)"
+      },
+      "width": {
+        "0": "var(--echoes-dimension-width-0, 0px)",
+        "25": "var(--echoes-dimension-width-25, 0.125rem)",
+        "50": "var(--echoes-dimension-width-50, 0.25rem)",
+        "75": "var(--echoes-dimension-width-75, 0.375rem)",
+        "100": "var(--echoes-dimension-width-100, 0.5rem)",
+        "150": "var(--echoes-dimension-width-150, 0.75rem)",
+        "200": "var(--echoes-dimension-width-200, 1rem)",
+        "250": "var(--echoes-dimension-width-250, 1.25rem)",
+        "300": "var(--echoes-dimension-width-300, 1.5rem)",
+        "400": "var(--echoes-dimension-width-400, 2rem)",
+        "450": "var(--echoes-dimension-width-450, 2.25rem)",
+        "500": "var(--echoes-dimension-width-500, 2.5rem)",
+        "600": "var(--echoes-dimension-width-600, 3rem)",
+        "700": "var(--echoes-dimension-width-700, 3.5rem)",
+        "800": "var(--echoes-dimension-width-800, 4rem)",
+        "5000": "var(--echoes-dimension-width-5000, 25rem)"
+      }
+    }
+  },
+  "echoesTypographyUtilities": {
+    ".code": {
+      "font": "var(--echoes-typography-code-default)"
+    },
+    ".code-highlight": {
+      "font": "var(--echoes-typography-code-highlight)"
+    },
+    ".code-comment": {
+      "font": "var(--echoes-typography-code-comment)"
+    },
+    ".heading-xs": {
+      "font": "var(--echoes-typography-heading-xsmall)"
+    },
+    ".heading-sm": {
+      "font": "var(--echoes-typography-heading-small)"
+    },
+    ".heading-md": {
+      "font": "var(--echoes-typography-heading-medium)"
+    },
+    ".heading-lg": {
+      "font": "var(--echoes-typography-heading-large)"
+    },
+    ".heading-xl": {
+      "font": "var(--echoes-typography-heading-xlarge)"
+    },
+    ".typo-default": {
+      "font": "var(--echoes-typography-text-default-regular)"
+    },
+    ".typo-semibold": {
+      "font": "var(--echoes-typography-text-default-semi-bold)"
+    },
+    ".typo-bold": {
+      "font": "var(--echoes-typography-text-default-bold)"
+    },
+    ".typo-sm": {
+      "font": "var(--echoes-typography-text-small-medium)"
+    },
+    ".typo-sm-semibold": {
+      "font": "var(--echoes-typography-text-small-semi-bold)"
+    },
+    ".typo-lg": {
+      "font": "var(--echoes-typography-text-large-regular)"
+    },
+    ".typo-lg-semibold": {
+      "font": "var(--echoes-typography-text-large-semi-bold)"
+    },
+    ".typo-label": {
+      "font": "var(--echoes-typography-others-label)"
+    },
+    ".typo-helper-text": {
+      "font": "var(--echoes-typography-others-helper-text)"
+    },
+    ".typo-display": {
+      "font": "var(--echoes-typography-display-default)"
+    }
+  }
+};
+
+export const echoesPreset = config.echoesPreset;
+export const echoesTypographyUtilities = config.echoesTypographyUtilities;
