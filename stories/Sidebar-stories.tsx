e226e363fdd881f57d3d10b651d8cc2c95967f1d
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2025-08-20 11:34:18 +0200

    ECHOES-906 Create SidebarNavigationItem component (#458)

---
+++ stories/Sidebar-stories.tsx
@@ -0,0 +1,45 @@
+/*
+ * Echoes React
+ * Copyright (C) 2023-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { IconOverview } from '../src';
+import { SidebarNavigationItem } from '../src/components/sidebar-navigation/SidebarNavigationItem';
+
+const meta: Meta<typeof SidebarNavigationItem> = {
+  component: SidebarNavigationItem,
+  title: 'Echoes/Sidebar',
+};
+
+export default meta;
+
+type Story = StoryObj<typeof SidebarNavigationItem>;
+
+export const NavigationItem: Story = {
+  args: {
+    Icon: IconOverview,
+    to: '/overview',
+    children: 'Item label',
+  },
+  render: (args) => (
+    <div style={{ width: '200px' }}>
+      <SidebarNavigationItem {...args} />
+    </div>
+  ),
+};
