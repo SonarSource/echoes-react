2d98ec907806c6e21fe0eca9e919026743216cef
amelia-adam-sonarsource <amelia.adam@sonarsource.com>
2026-09-23 11:00:45 -0500

    SC-58138 Add IconContainer

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -48,6 +48,7 @@ export { IconCodeSmell } from './IconCodeSmell';
 export { IconCollapse } from './IconCollapse';
 export { IconComment } from './IconComment';
 export { IconComputer } from './IconComputer';
+export { IconContainer } from './IconContainer';
 export { IconContentCut } from './IconContentCut';
 export { IconContract } from './IconContract';
 export { IconCopy } from './IconCopy';
